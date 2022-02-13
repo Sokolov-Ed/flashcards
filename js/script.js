cc87553f4e7a9b6ef949cc792f264d84b6941061
Sokolov Eduard <Edsokolov30@gmail.com>
2022-02-13 22:39:07 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,58 @@
+let addCard = document.getElementById("addCard");
+let deleteCards = document.getElementById("deleteCards");
+let quastion = document.getElementById("quastion");
+let answer = document.getElementById("answer");
+let save = document.getElementById("save");
+let close = document.getElementById("close");
+let cardsField = document.getElementsByClassName("cardsField")[0];
+let cardCreateField = document.getElementsByClassName("cardCreateField")[0];
+let allCards = [];
+
+if(localStorage.getItem('cards')) {
+	for(let i = 0; i < JSON.parse(localStorage.getItem('cards')).length; i++) {
+		allCards.push(JSON.parse(localStorage.getItem('cards'))[i]);
+		cardsField.innerHTML += `<div class="card">
+									<div class="answerCardClosed">${allCards[i]["answer"]}</div>
+									<div class="quastionCard">${allCards[i]["quastion"]}</div>
+								</div>`;
+	}
+}
+
+addCard.addEventListener('click', e => {
+	cardCreateField.classList.toggle("cardCreateFieldOpened");
+	addCard.style.pointerEvents = "none";
+})
+
+close.addEventListener('click', e => {
+	cardCreateField.classList.toggle("cardCreateFieldOpened");
+	addCard.style.pointerEvents = "auto";
+	answer.value = "";
+	quastion.value = "";
+})
+
+deleteCards.addEventListener('click', e => {
+	cardsField.innerHTML = "";
+	localStorage.clear();
+	allCards = [];
+})
+
+save.addEventListener('click', e => {
+	answer.value.trim() === '' ? answer.classList.add("valueEmpty") : answer.classList.remove("valueEmpty");
+	quastion.value.trim() === '' ? quastion.classList.add("valueEmpty") : quastion.classList.remove("valueEmpty");
+	if (answer.value.trim() !== '' && quastion.value.trim() !== '') {
+		allCards.push({"quastion": quastion.value, "answer":  answer.value});
+		localStorage.setItem('cards', JSON.stringify(allCards));
+		cardsField.innerHTML += `<div class="card">
+									<div class="answerCardClosed">${answer.value}</div>
+									<div class="quastionCard">${quastion.value}</div>
+								</div>`;
+		answer.value = "";
+		quastion.value = "";
+	}
+})
+
+cardsField.addEventListener('click', e => {
+	if(e.target.closest('div').className === "card") {
+		e.target.children[0].classList.toggle("answerCard");
+	}
+})
