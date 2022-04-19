f97379f580359fb659bbff1135955e1b4a4a2cc0
Sokolov-Ed <edsokolov30@gmail.com>
2022-04-19 14:35:29 +0300

    changing attribute names

--- js/script.js
+++ js/script.js
@@ -1,30 +1,30 @@
-let addCard = document.getElementById("addCard");
-let deleteCards = document.getElementById("deleteCards");
+let addCard = document.getElementById("add_card");
+let deleteCards = document.getElementById("delete_cards");
 let quastion = document.getElementById("quastion");
 let answer = document.getElementById("answer");
 let save = document.getElementById("save");
 let close = document.getElementById("close");
-let cardsField = document.getElementsByClassName("cardsField")[0];
-let cardCreateField = document.getElementsByClassName("cardCreateField")[0];
+let cardsField = document.getElementsByClassName("cards_field")[0];
+let cardCreateField = document.getElementsByClassName("card_create_field")[0];
 let allCards = [];
 
 if(localStorage.getItem('cards')) {
 	for(let i = 0; i < JSON.parse(localStorage.getItem('cards')).length; i++) {
 		allCards.push(JSON.parse(localStorage.getItem('cards'))[i]);
 		cardsField.innerHTML += `<div class="card">
-									<div class="answerCardClosed">${allCards[i]["answer"]}</div>
-									<div class="quastionCard">${allCards[i]["quastion"]}</div>
+									<div class="answer_card_closed">${allCards[i]["answer"]}</div>
+									<div class="quastion_card">${allCards[i]["quastion"]}</div>
 								</div>`;
 	}
 }
 
 addCard.addEventListener('click', e => {
-	cardCreateField.classList.toggle("cardCreateFieldOpened");
+	cardCreateField.classList.toggle("card_create_field_opened");
 	addCard.style.pointerEvents = "none";
 })
 
 close.addEventListener('click', e => {
-	cardCreateField.classList.toggle("cardCreateFieldOpened");
+	cardCreateField.classList.toggle("card_create_field_opened");
 	addCard.style.pointerEvents = "auto";
 	answer.value = "";
 	quastion.value = "";
@@ -38,23 +38,23 @@ deleteCards.addEventListener('click', e => {
 
 save.addEventListener('click', e => {
 	if(answer.value.trim() === '') {
-		answer.classList.add("valueEmpty");
+		answer.classList.add("value_empty");
 	}
 	else {
-		answer.classList.remove("valueEmpty");
+		answer.classList.remove("value_empty");
 	}
 	if(quastion.value.trim() === '') {
-		quastion.classList.add("valueEmpty");
+		quastion.classList.add("value_empty");
 	}
 	else {
-		quastion.classList.remove("valueEmpty");
+		quastion.classList.remove("value_empty");
 	}
 	if (answer.value.trim() !== '' && quastion.value.trim() !== '') {
 		allCards.push({"quastion": quastion.value, "answer":  answer.value});
 		localStorage.setItem('cards', JSON.stringify(allCards));
 		cardsField.innerHTML += `<div class="card">
-									<div class="answerCardClosed">${answer.value}</div>
-									<div class="quastionCard">${quastion.value}</div>
+									<div class="answer_card_closed">${answer.value}</div>
+									<div class="quastion_card">${quastion.value}</div>
 								</div>`;
 		answer.value = "";
 		quastion.value = "";
@@ -63,6 +63,6 @@ save.addEventListener('click', e => {
 
 cardsField.addEventListener('click', e => {
 	if(e.target.closest('div').className === "card") {
-		e.target.children[0].classList.toggle("answerCard");
+		e.target.children[0].classList.toggle("answer_card");
 	}
 })
